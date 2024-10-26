bacc2511bcce8016b2afbbd87af600ec5bcfba23
Volodymir Ihnatsevych <ukakyn@gmail.com>
2024-10-26 14:08:59 +0300

    Added new class User

---
+++ user.h
@@ -0,0 +1,21 @@
+#ifndef USER_H
+#define USER_H
+
+#include "header_lib.h"
+
+class User
+{
+private:
+	int id;
+	string email;
+	string password;
+	string name;
+	string surname;
+public:
+	User()
+	{
+
+	}
+};
+
+#endif
